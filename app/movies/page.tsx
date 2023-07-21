9835d2a78db107b53e219056f0f23d1d3638e0d9
anhhuy <phamanhhuy22@gmail.com>
2023-07-22 01:42:52 +0700

    error page

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -2,14 +2,12 @@
 import BillBoard from "@/components/BillBoard";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
-import { useFavorites } from "@/hook/useFavorites";
 import { useMovieList } from "@/hook/useMovieList";
 import { useSession } from "next-auth/react";
 
 function Movies() {
 	const session = useSession();
 	const { data: movieList, isLoading } = useMovieList();
-	const { data: favoriteMovies } = useFavorites(session.data?.user?.email!);
 
 	return (
 		<div className="relative">
